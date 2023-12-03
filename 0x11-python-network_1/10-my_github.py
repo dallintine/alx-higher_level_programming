4b315782ba57379781fb187644131b014013b99d
dallintine <dallintine@yahoo.ca>
2023-12-03 09:46:43 -0800

    lastupdate

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
+"""script that takes your GitHub credentials (username and password) and
+uses the GitHub API to display your id.
+The first argument will be your username.
+The second argument will be your password (in your case, a personal
+access token as password).
 """
-takes your Github credentials (username and password) and uses the
-Github API to display your id
-"""
+import sys
 import requests
-from sys import argv
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    """
-    takes your Github credentials (username and password) and uses the
-    Github API to display your id
-    """
-    username = argv[1]
-    password = argv[2]
-    url = 'https://api.github.com/user'
-    r = requests.get(url, auth=(username, password))
-    try:
-        print(r.json().get('id'))
-    except:
-        pass
+    url = "https://api.github.com/user"
+    username = sys.argv[1]
+    password = sys.argv[2]
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+    json_obj = response.json()
+    print(json_obj.get("id"))
--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
 """
-list 10 commits (from the most recent to oldest) of the repository “rails”
-by the user “rails”
+script that takes 2 arguments in order to list 10 commits (from the most
+recent to oldest) of the repository "rails" by the user "rails".
+Print all commits by: `<sha>: <author name>` (one by line)
+The first argument will be the repository name
+The second argument will be the owner name
 """
+import sys
 import requests
-from sys import argv
 
 
 if __name__ == "__main__":
-    """
-    list 10 commits (from the most recent to oldest) of the repository
-    “rails” by the user “rails”
-    """
-    repo = argv[1]
-    owner = argv[2]
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-    r = requests.get(url)
-    res_list = r.json()
     try:
-        for i in range(10):
-            print("{}: {}".format(res_list[i].get('sha'), res_list[i].
-                                  get('commit').get('author').get('name')))
-    except:
+        repo_name = sys.argv[1]
+        username = sys.argv[2]
+        commmits_url = "https://api.github.com/repos/{}/{}/commits" \
+            .format(username, repo_name)
+        response = requests.get(commmits_url)
+        json_obj = response.json()
+        for i, obj in enumerate(json_obj):
+            if i == 10:
+                break
+            if type(obj) is dict:
+                name = obj.get('commit').get('author').get('name')
+                print("{}: {}".format(obj.get('sha'), name))
+    except ValueError as invalid_json:
         pass
